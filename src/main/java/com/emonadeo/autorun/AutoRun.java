1209da705cf29d015320488419c4146a108ea686
Emanuel Pilz <emonadeo@gmail.com>
2020-03-16 01:49:24 +0100

    Added Auto-Run Mixin

--- src/main/java/com/emonadeo/autorun/AutoRun.java
+++ src/main/java/com/emonadeo/autorun/AutoRun.java
@@ -1,14 +1,42 @@
 package com.emonadeo.autorun;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
+import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class AutoRun implements ModInitializer {
+
+    private static FabricKeyBinding keyBinding;
+    private static boolean toggled;
+
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
+        AutoRun.toggled = false;
+        AutoRun.keyBinding = FabricKeyBinding.Builder.create(
+                new Identifier("autorun"),
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_P, // Default to 'p'
+                "key.categories.movement" // Append movement category
+        ).build();
 
-        System.out.println("Hello Fabric world!");
+        // Register Keybinding
+        KeyBindingRegistry.INSTANCE.register(keyBinding);
+
+        ClientTickCallback.EVENT.register(client -> {
+            if (keyBinding.wasPressed()) {
+                toggled = !toggled;
+            }
+            else if (client.options.keyBack.wasPressed()) {
+                toggled = false;
+            }
+        });
+    }
+
+    public static boolean isToggled() {
+        return toggled;
     }
 }
---
+++ src/main/java/com/emonadeo/autorun/mixin/AutoRunMixin.java
@@ -0,0 +1,22 @@
+package com.emonadeo.autorun.mixin;
+
+import com.emonadeo.autorun.AutoRun;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.input.KeyboardInput;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Environment(EnvType.CLIENT)
+@Mixin(KeyboardInput.class)
+public class AutoRunMixin {
+
+    private boolean pressingForward;
+
+    @Redirect(method = "tick", at = @At(value = "FIELD", target = "net/minecraft/client/input/KeyboardInput.pressingForward:Z", opcode = Opcodes.GETFIELD))
+    private boolean onPressingForwardInTick(KeyboardInput input) {
+        return this.pressingForward || AutoRun.isToggled();
+    }
+}
