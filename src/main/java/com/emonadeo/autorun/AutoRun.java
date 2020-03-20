5760fdd304d7a81fec11911f0d86a234c6b626b4
Emanuel Pilz <emonadeo@gmail.com>
2020-03-20 17:05:23 +0100

    Implemented activation of omnidirectional Auto-Run

--- src/main/java/com/emonadeo/autorun/AutoRun.java
+++ src/main/java/com/emonadeo/autorun/AutoRun.java
@@ -4,18 +4,25 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AutoRun implements ModInitializer {
 
     private static FabricKeyBinding keyBinding;
-    private static boolean toggled;
+    private static Set<MovementDirection> toggled;
 
     @Override
     public void onInitialize() {
-        AutoRun.toggled = false;
+        AutoRun.toggled = new HashSet<>();
         AutoRun.keyBinding = FabricKeyBinding.Builder.create(
                 new Identifier("autorun", "toggle"),
                 InputUtil.Type.KEYSYM,
@@ -28,15 +35,29 @@ public class AutoRun implements ModInitializer {
 
         ClientTickCallback.EVENT.register(client -> {
             if (keyBinding.wasPressed()) {
-                toggled = !toggled;
-            }
-            else if (client.options.keyBack.wasPressed() || client.options.keyForward.wasPressed()) {
-                toggled = false;
+                boolean activating = toggled.isEmpty();
+
+                if (!activating) {
+                    // Deactivating
+                    toggled.clear();
+                } else {
+                    // Activating
+                    Set<MovementDirection> pressedDirections = Arrays.stream(MovementDirection.values())
+                            .filter(dir -> dir.getKeyBinding(client).isPressed()).collect(Collectors.toSet());
+
+                    if (!pressedDirections.isEmpty()) {
+                        // Activate pressed directions
+                        toggled.addAll(pressedDirections);
+                    } else {
+                        // Activate forward by default
+                        toggled.add(MovementDirection.FORWARD);
+                    }
+                }
             }
         });
     }
 
-    public static boolean isToggled() {
+    public static Set<MovementDirection> getToggled() {
         return toggled;
     }
 }
--- src/main/java/com/emonadeo/autorun/mixin/AutoRunMixin.java
+++ src/main/java/com/emonadeo/autorun/mixin/AutoRunMixin.java
@@ -1,6 +1,7 @@
 package com.emonadeo.autorun.mixin;
 
 import com.emonadeo.autorun.AutoRun;
+import com.emonadeo.autorun.MovementDirection;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.input.KeyboardInput;
@@ -14,7 +15,22 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class AutoRunMixin {
 
     @Redirect(method = "tick", at = @At(value = "FIELD", target = "net/minecraft/client/input/KeyboardInput.pressingForward:Z", opcode = Opcodes.GETFIELD))
-    private boolean onPressingForwardInTick(KeyboardInput input) {
-        return input.pressingForward || AutoRun.isToggled();
+    private boolean onPressingForward(KeyboardInput input) {
+        return input.pressingForward || AutoRun.getToggled().contains(MovementDirection.FORWARD);
+    }
+
+    @Redirect(method = "tick", at = @At(value = "FIELD", target = "net/minecraft/client/input/KeyboardInput.pressingBack:Z", opcode = Opcodes.GETFIELD))
+    private boolean onPressingBack(KeyboardInput input) {
+        return input.pressingBack || AutoRun.getToggled().contains(MovementDirection.BACK);
+    }
+
+    @Redirect(method = "tick", at = @At(value = "FIELD", target = "net/minecraft/client/input/KeyboardInput.pressingLeft:Z", opcode = Opcodes.GETFIELD))
+    private boolean onPressingLeft(KeyboardInput input) {
+        return input.pressingLeft || AutoRun.getToggled().contains(MovementDirection.LEFT);
+    }
+
+    @Redirect(method = "tick", at = @At(value = "FIELD", target = "net/minecraft/client/input/KeyboardInput.pressingRight:Z", opcode = Opcodes.GETFIELD))
+    private boolean onPressingRight(KeyboardInput input) {
+        return input.pressingRight || AutoRun.getToggled().contains(MovementDirection.RIGHT);
     }
 }
